9a9a62582426d59b957efa6315d914d257750fca
karthikp <karthikp.binary@gmail.com>
2022-02-05 14:51:23 +0530

    implemented interceptors and data serialization

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,13 +7,18 @@ import {
   Param,
   Query,
   Delete,
-  NotFoundException
+  NotFoundException,
+  UseInterceptors,
+  ClassSerializerInterceptor
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
@@ -25,6 +30,7 @@ export class UsersController {
 
   @Get('/:id')
   async findUser(@Param('id') id: string) {
+    console.log('handler is running');
     const user = await this.usersService.findOne(parseInt(id));
 
     if (!user) {
